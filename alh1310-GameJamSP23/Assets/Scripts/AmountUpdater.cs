01d6951e3b56852c50efe9bf34b623b2e2c2a066
AnneH20 <alh1310@msstate.edu>
2023-03-25 00:28:26 -0500

    Building PlayGame Scene

---
+++ alh1310-GameJamSP23/Assets/Scripts/AmountUpdater.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Globalization;
+
+public class AmountUpdater : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    private int number = 0;
+    private string scoreKey = "Number";
+
+    private void Start()
+    {
+        number = PlayerPrefs.GetInt(scoreKey, 0);
+        UpdateText();
+        Debug.Log("Start");
+    }
+
+    public void IncreaseNumber()
+    {
+        number++;
+        PlayerPrefs.SetInt(scoreKey, number);
+        UpdateText();
+        Debug.Log("Increase");
+    }
+
+    public void DecreaseNumber()
+    {
+        if (number > 0)
+        {
+            number--;
+            PlayerPrefs.SetInt(scoreKey, number);
+            UpdateText();
+            Debug.Log("Decrease");
+        }
+
+    }
+
+    public void ResetScore()
+    {
+        PlayerPrefs.SetInt(scoreKey, 0);
+        number = 0;
+        UpdateText();
+        Debug.Log("Reset");
+    }
+
+    private void UpdateText()
+    {
+        textDisplay.text = number.ToString();
+        Debug.Log("Update");
+    }
+}
---
+++ alh1310-GameJamSP23/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public AmountUpdater pennyUpdater;
+    public AmountUpdater nickelUpdater;
+    public AmountUpdater dimeUpdater;
+    public AmountUpdater quarterUpdater;
+    public AmountUpdater halfdollarUpdater;
+
+    public AmountUpdater oneUpdater;
+    public AmountUpdater fiveUpdater;
+    public AmountUpdater tenUpdater;
+    public AmountUpdater twentyUpdater;
+    public AmountUpdater fiftyUpdater;
+    public AmountUpdater hundredUpdater;
+
+
+    public GameObject settingsMenu;
+
+    public GameObject amountTitle;
+    public GameObject amountPaid;
+    public GameObject changeAmount;
+    public GameObject submitButton;
+
+    public GameObject pennyAmount;
+    public GameObject nickelAmount;
+    public GameObject dimeAmount;
+    public GameObject quarterAmount;
+    public GameObject halfdollarAmount;
+
+    public GameObject oneAmount;
+    public GameObject fiveAmount;
+    public GameObject tenAmount;
+    public GameObject twentyAmount;
+    public GameObject fiftyAmount;
+    public GameObject hundredAmount;
+
+    public static bool isPaused;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        pennyUpdater.ResetScore();
+        nickelUpdater.ResetScore();
+        dimeUpdater.ResetScore();
+        quarterUpdater.ResetScore();
+        halfdollarUpdater.ResetScore();
+
+        oneUpdater.ResetScore();
+        fiveUpdater.ResetScore();
+        tenUpdater.ResetScore();
+        twentyUpdater.ResetScore();
+        fiftyUpdater.ResetScore();
+        hundredUpdater.ResetScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SettingsMenu()
+    {
+        settingsMenu.SetActive(true);
+
+        amountTitle.SetActive(false);
+        amountPaid.SetActive(false);
+        changeAmount.SetActive(false);
+        submitButton.SetActive(false);
+
+        pennyAmount.SetActive(false);
+        nickelAmount.SetActive(false);
+        dimeAmount.SetActive(false);
+        quarterAmount.SetActive(false);
+        halfdollarAmount.SetActive(false);
+
+        oneAmount.SetActive(false);
+        fiveAmount.SetActive(false);
+        tenAmount.SetActive(false);
+        twentyAmount.SetActive(false);
+        fiftyAmount.SetActive(false);
+        hundredAmount.SetActive(false);
+
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        settingsMenu.SetActive(false);
+
+        amountTitle.SetActive(true);
+        amountPaid.SetActive(true);
+        changeAmount.SetActive(true);
+        submitButton.SetActive(true);
+
+        pennyAmount.SetActive(true);
+        nickelAmount.SetActive(true);
+        dimeAmount.SetActive(true);
+        quarterAmount.SetActive(true);
+        halfdollarAmount.SetActive(true);
+
+        oneAmount.SetActive(true);
+        fiveAmount.SetActive(true);
+        tenAmount.SetActive(true);
+        twentyAmount.SetActive(true);
+        fiftyAmount.SetActive(true);
+        hundredAmount.SetActive(true);
+
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
